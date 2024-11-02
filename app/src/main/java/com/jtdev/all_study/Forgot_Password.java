0070e85da317a8525e8dd5bcd77feae19e35ceff
Reignxxds1 <dahunogreignier@gmail.com>
2024-11-02 10:15:44 +0800

    Fixed the backbuttons and added functionalities of forgotpassword OTP and newpassword

--- app/src/main/java/com/jtdev/all_study/Forgot_Password.java
+++ app/src/main/java/com/jtdev/all_study/Forgot_Password.java
@@ -1,6 +1,11 @@
 package com.jtdev.all_study;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Forgot_Password extends AppCompatActivity {
-
+private Button send;
+private EditText email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_08_forgot_password);
+
+        Button send = findViewById(R.id.send);
+        EditText email = findViewById(R.id.email);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        send.setOnClickListener(v -> {
+            Intent intent = new Intent(Forgot_Password.this, OTP_AllStudy.class);
+            Toast.makeText(Forgot_Password.this, "OTP Sent!", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+
         });
     }
 }
--- app/src/main/java/com/jtdev/all_study/New_Password.java
+++ app/src/main/java/com/jtdev/all_study/New_Password.java
@@ -1,6 +1,10 @@
 package com.jtdev.all_study;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class New_Password extends AppCompatActivity {
+private Button confirm_button;
+private EditText newpassword;
+private EditText confirmpassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_10_new_password);
+
+        Button confirm_button = findViewById(R.id.confirm_button);
+        EditText newpassword = findViewById(R.id.newpassword);
+        EditText confirmpassword = findViewById(R.id.confirmpassword);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        confirm_button.setOnClickListener(v -> {
+            Toast.makeText(this, "Password Changed!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(New_Password.this, Login.class);
+            startActivity(intent);
         });
     }
 }
