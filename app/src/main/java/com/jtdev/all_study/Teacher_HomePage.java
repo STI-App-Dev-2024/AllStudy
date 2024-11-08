59bb564700b2959926d63db7cf5e48a270e5f8c7
Nowell Intia <intia.318145@dasmarinas.sti.edu.ph>
2024-11-08 12:12:27 +0800

    Added Teacher see more page

--- app/src/main/java/com/jtdev/all_study/Teacher_HomePage.java
+++ app/src/main/java/com/jtdev/all_study/Teacher_HomePage.java
@@ -1,6 +1,8 @@
 package com.jtdev.all_study;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,20 @@ public class Teacher_HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_13_teacher_home_page);
+
+        Button join_now_button = findViewById(R.id.join_now_button);
+        Button see_more_button = findViewById(R.id.see_more_button);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        see_more_button.setOnClickListener(v ->{
+            Intent intent = new Intent(Teacher_HomePage.this, Teacher_SeeMore.class);
+            startActivity(intent);
+        });
+
     }
 }
