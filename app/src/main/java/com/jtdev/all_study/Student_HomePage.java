f61a6c7b981cbf508e651089aa4bc506200b1910
Nowell Intia <intia.318145@dasmarinas.sti.edu.ph>
2024-11-08 12:39:57 +0800

    Instantiated the necessary buttons and add some backend functions

--- app/src/main/java/com/jtdev/all_study/Student_HomePage.java
+++ app/src/main/java/com/jtdev/all_study/Student_HomePage.java
@@ -1,6 +1,9 @@
 package com.jtdev.all_study;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,23 @@ public class Student_HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_12_student_homepage);
+
+        // Instantiated all buttons by getting their IDs from student_homepage.xml
+        Button join_now_button = findViewById(R.id.join_now_button);
+        Button see_more_button = findViewById(R.id.see_more_button);
+        ImageButton notification_button = findViewById(R.id.notification_button);
+        ImageButton join_server_button = findViewById(R.id.join_server_button);
+        ImageButton favorite_button = findViewById(R.id.favorite_button);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        see_more_button.setOnClickListener(v -> {
+            Intent intent = new Intent(Student_HomePage.this, Student_SeeMore.class);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/jtdev/all_study/Student_SeeMore.java
+++ app/src/main/java/com/jtdev/all_study/Student_SeeMore.java
@@ -1,6 +1,8 @@
 package com.jtdev.all_study;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,16 @@ public class Student_SeeMore extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_student_see_more);
+
+        Button join_now_button = findViewById(R.id.join_now_button);
+        Button join_now_button_2 = findViewById(R.id.join_now_button_2);
+        ImageButton join_server_button = findViewById(R.id.join_server_button);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
