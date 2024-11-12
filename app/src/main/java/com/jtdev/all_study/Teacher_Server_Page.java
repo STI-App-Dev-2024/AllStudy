24c5fecf6b7e7b50be68431272d88bbae9ffe30b
Nowell Intia <intia.318145@dasmarinas.sti.edu.ph>
2024-11-12 21:27:23 +0800

    Added confirm button class

--- app/src/main/java/com/jtdev/all_study/Teacher_Server_Page.java
+++ app/src/main/java/com/jtdev/all_study/Teacher_Server_Page.java
@@ -1,6 +1,11 @@
 package com.jtdev.all_study;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import com.jtdev.all_study.Confirm_Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,14 @@ public class Teacher_Server_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_teacher_server_page);
+
+        ImageButton logout_button = findViewById(R.id.logout_button);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
