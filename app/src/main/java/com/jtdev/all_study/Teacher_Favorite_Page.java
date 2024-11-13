c195d9a497867c525e46ed6b48db19c159b80fa8
Nowell Intia <intia.318145@dasmarinas.sti.edu.ph>
2024-11-13 20:39:25 +0800

    Added another page and some UI elements in other pages

--- app/src/main/java/com/jtdev/all_study/Teacher_Favorite_Page.java
+++ app/src/main/java/com/jtdev/all_study/Teacher_Favorite_Page.java
@@ -1,9 +1,6 @@
 package com.jtdev.all_study;
 
-import android.media.Image;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,21 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Teacher_See_More extends AppCompatActivity {
+public class Teacher_Favorite_Page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_teacher_see_more);
-
-        ImageButton edit_profile_button = findViewById(R.id.edit_profile_button);
-        Button join_now_button_1 = findViewById(R.id.join_now_button_1);
-        Button join_now_button_2 = findViewById(R.id.join_now_button_2);
-        ImageButton notification_button = findViewById(R.id.notification_button);
-        ImageButton create_server_button = findViewById(R.id.create_server_button);
-        ImageButton favorite_button = findViewById(R.id.favorite_button);
-
+        setContentView(R.layout.activity_teacher_favorite_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/jtdev/all_study/Teacher_See_More_Page.java
+++ app/src/main/java/com/jtdev/all_study/Teacher_See_More_Page.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,18 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Teacher_Inside_Server_Page extends AppCompatActivity {
+public class Teacher_See_More_Page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_teacher_inside_server_page);
+        setContentView(R.layout.activity_teacher_see_more_page);
 
         ImageButton edit_profile_button = findViewById(R.id.edit_profile_button);
-        Button see_activities_button = findViewById(R.id.see_activities_button);
-        TextView see_classroom_rules_click = findViewById(R.id.see_classroom_rules_click);
-        ImageButton logout_button = findViewById(R.id.logout_button);
+        Button join_now_button_1 = findViewById(R.id.join_now_button_1);
+        Button join_now_button_2 = findViewById(R.id.join_now_button_2);
+        ImageButton notification_button = findViewById(R.id.notification_button);
+        ImageButton create_server_button = findViewById(R.id.create_server_button);
+        ImageButton favorite_button = findViewById(R.id.favorite_button);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -32,19 +33,18 @@ public class Teacher_Inside_Server_Page extends AppCompatActivity {
         });
 
         edit_profile_button.setOnClickListener(v -> {
-            Intent intent = new Intent (Teacher_Inside_Server_Page.this, Teacher_Profile_Page.class);
+            Intent intent = new Intent(Teacher_See_More_Page.this, Teacher_Profile_Page.class);
             startActivity(intent);
         });
 
-        see_activities_button.setOnClickListener(v -> {
-            Intent intent = new Intent(Teacher_Inside_Server_Page.this, Teacher_See_More_Page.class);
-            startActivity(intent);
-            });
-
-        see_classroom_rules_click.setOnClickListener(v -> {
-            Intent intent = new Intent(Teacher_Inside_Server_Page.this, Teacher_Classroom_Rules.class);
+        notification_button.setOnClickListener(v -> {
+            Intent intent = new Intent(Teacher_See_More_Page.this, Teacher_Notification_Page.class);
             startActivity(intent);
         });
 
+        favorite_button.setOnClickListener(v -> {
+            Intent intent = new Intent(Teacher_See_More_Page.this, Teacher_Favorite_Page.class);
+            startActivity(intent);
+        });
     }
 }
