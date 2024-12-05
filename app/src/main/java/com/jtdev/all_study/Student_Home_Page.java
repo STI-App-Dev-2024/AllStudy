9bdb76b540dd080af759dec01eb851cfb13a90b5
Nowell Intia <intia.318145@dasmarinas.sti.edu.ph>
2024-12-05 12:10:12 +0800

    Added code and reduce code that may affect the behavior of buttons

--- app/src/main/java/com/jtdev/all_study/Student_Home_Page.java
+++ app/src/main/java/com/jtdev/all_study/Student_Home_Page.java
@@ -20,12 +20,12 @@ public class Student_Home_Page extends AppCompatActivity {
         setContentView(R.layout.activity_12_student_home_page);
 
         // Instantiated all buttons by getting their IDs from student_homepage.xml
+        ImageButton edit_profile_button = findViewById(R.id.edit_profile_button);
         Button join_now_button = findViewById(R.id.join_now_button);
         Button see_more_button = findViewById(R.id.see_more_button);
         ImageButton notification_button = findViewById(R.id.notification_button);
         ImageButton join_server_button = findViewById(R.id.join_server_button);
         ImageButton favorite_button = findViewById(R.id.favorite_button);
-        ImageButton edit_profile_button = findViewById(R.id.edit_profile_button);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -33,35 +33,34 @@ public class Student_Home_Page extends AppCompatActivity {
             return insets;
         });
 
-        see_more_button.setOnClickListener(v -> {
-            Intent intent = new Intent(Student_Home_Page.this, Student_See_More_Page.class);
-            startActivity(intent);
-        });
-
-        join_now_button.setOnClickListener(v -> {
-            Intent intent = new Intent(Student_Home_Page.this, Student_Inside_Server_Page.class);
-            startActivity(intent);
-        });
-
-        notification_button.setOnClickListener(v -> {
-            Intent intent = new Intent(Student_Home_Page.this, Student_Notification_Page.class);
-            startActivity(intent);
-        });
-
-        edit_profile_button.setOnClickListener(v -> {
+        edit_profile_button.setOnClickListener(v ->{
             Intent intent = new Intent(Student_Home_Page.this, Student_Edit_Profile_Page.class);
             startActivity(intent);
         });
 
-        join_server_button.setOnClickListener(v -> {
+        join_now_button.setOnClickListener(v ->{
+            Intent intent = new Intent(Student_Home_Page.this, Student_Inside_Server_Page.class);
+            startActivity(intent);
+        });
+
+        see_more_button.setOnClickListener(v ->{
             Intent intent = new Intent(Student_Home_Page.this, Student_See_More_Page.class);
             startActivity(intent);
         });
 
-        favorite_button.setOnClickListener(v -> {
-            Intent intent = new Intent(Student_Home_Page.this, Student_Favorite_Server_Page.class);
+        notification_button.setOnClickListener(v ->{
+            Intent intent = new Intent(Student_Home_Page.this, Student_Notification_Page.class);
             startActivity(intent);
         });
 
-        }
+        join_server_button.setOnClickListener(v ->{
+            Intent intent = new Intent(Student_Home_Page.this, Student_See_More_Page.class);
+            startActivity(intent);
+        });
+
+        favorite_button.setOnClickListener(v ->{
+            Intent intent = new Intent(Student_Home_Page.this, Student_Favorite_Server_Page.class);
+            startActivity(intent);
+        });
     }
+}
